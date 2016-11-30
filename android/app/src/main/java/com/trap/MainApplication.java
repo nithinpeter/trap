09f046e615b030b0e22089f4cf264ad4b682227d
Nithin Peter <nithinpeter7@gmail.com>
2016-11-30 21:17:00 +1100

    Andorid HockeyApp integration.

--- android/app/src/main/java/com/trap/MainApplication.java
+++ android/app/src/main/java/com/trap/MainApplication.java
@@ -13,6 +13,9 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.List;
 
+import com.slowpath.hockeyapp.RNHockeyAppModule; // <--- import
+import com.slowpath.hockeyapp.RNHockeyAppPackage;  // <--- import
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
@@ -24,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
+          new RNHockeyAppPackage(MainApplication.this),
           new MainReactPackage(),
             new MapsPackage()
       );
